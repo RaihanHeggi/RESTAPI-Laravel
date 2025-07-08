1e4f68c7fea44023ac1cb224117f68b0673aad23
Sya Raihan Heggi <58820833+RaihanHeggi@users.noreply.github.com>
2025-07-08 18:12:13 +0700

    08/07/2025

---
+++ database/migrations/2025_07_08_010522_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('guid');
+            $table->string('name');
+            $table->integer('stocks');
+            $table->enum('active', ['Active','Not Active']);
+            $table->string('createdBy');
+            $table->string('updatedBy');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,5 @@ Route::post('/signin', [AuthController::class, 'index'])->name('signin');
 Route::post('/register', [AuthController::class, 'index'])->name('signup');
 
 Route::middleware('auth:sanctum')->group( function () {
-    // Route::resource('products', ProductController::class);
+    Route::resource('/products', ProductController::class);
 });
